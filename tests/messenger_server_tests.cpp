f63937b65f7121067faeacec05242586fbefb92b
aliensol <aliensol@mail.ru>
2021-09-01 21:13:29 +0300

    add tests

---
+++ tests/messenger_server_tests.cpp
@@ -0,0 +1,15 @@
+#define BOOST_TEST_MODULE test_messenger_server
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_messenger_server)
+
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+    BOOST_CHECK(true);
+}
+
+}
+
+
+//int main() {}
